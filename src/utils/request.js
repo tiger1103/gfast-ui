a27aa20e5850dc2343b5a6d7517dc6da90a1fedc
yxh <yxh1103@qq.com>
2022-01-06 10:26:09 +0800

    reject api接口返回的具体错误信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,45 +21,45 @@ service.interceptors.request.use(config => {
   }
   return config
 }, error => {
-    console.log(error)
-    Promise.reject(error)
+  console.log(error)
+  Promise.reject(error)
 })
 
 // 响应拦截器
 service.interceptors.response.use(res => {
-    // 未设置状态码则默认成功状态
-    const code = res.data.code || 0;
-    // 获取错误信息
-    const message = errorCode[code] || res.data.msg || errorCode['default']
-    if (code === -401) {
-      MessageBox.confirm(
-        '登录状态已过期，您可以继续留在该页面，或者重新登录',
-        '系统提示',
-        {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }
-      ).then(() => {
-        store.dispatch('LogOut').then(() => {
-          location.reload() // 为了重新实例化vue-router对象 避免bug
-        })
+  // 未设置状态码则默认成功状态
+  const code = res.data.code || 0;
+  // 获取错误信息
+  const message = errorCode[code] || res.data.msg || errorCode['default']
+  if (code === -401) {
+    MessageBox.confirm(
+      '登录状态已过期，您可以继续留在该页面，或者重新登录',
+      '系统提示',
+      {
+        confirmButtonText: '重新登录',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }
+    ).then(() => {
+      store.dispatch('LogOut').then(() => {
+        location.reload() // 为了重新实例化vue-router对象 避免bug
       })
-    } else if (code === 500) {
-      Message({
-        message: message,
-        type: 'error'
-      })
-      return Promise.reject(new Error(message))
-    } else if (code !== 0) {
-      Notification.error({
-        title: message
-      })
-      return Promise.reject('error')
-    } else {
-      return res.data
-    }
-  },
+    })
+  } else if (code === 500) {
+    Message({
+      message: message,
+      type: 'error'
+    })
+    return Promise.reject(new Error(message))
+  } else if (code !== 0) {
+    Notification.error({
+      title: message
+    })
+    return Promise.reject(res.data)
+  } else {
+    return res.data
+  }
+},
   error => {
     console.log('err' + error)
     Message({
