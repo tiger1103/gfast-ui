f2c0a1e640b53c147fb50feaf95627cb2d27c17b
yxh <yxh1103@qq.com>
2023-01-29 17:45:08 +0800

    升级element-plus

--- src/layout/main/defaults.vue
+++ src/layout/main/defaults.vue
@@ -1,47 +1,48 @@
 <template>
-	<el-container class="layout-container">
-		<Aside />
-		<el-container class="flex-center" :class="{ 'layout-backtop': !isFixedHeader }">
-			<Header v-if="isFixedHeader" />
-			<el-scrollbar ref="layoutDefaultsScrollbarRef" :class="{ 'layout-backtop': isFixedHeader }">
-				<Header v-if="!isFixedHeader" />
-				<Main />
-			</el-scrollbar>
-		</el-container>
-		<el-backtop target=".layout-backtop .el-scrollbar__wrap"></el-backtop>
-	</el-container>
+  <el-container class="layout-container">
+    <layoutAside />
+    <el-container class="flex-center" :class="{ 'layout-backtop': !isFixedHeader }">
+      <layoutHeader v-if="isFixedHeader" />
+      <el-scrollbar ref="layoutDefaultsScrollbarRef" :class="{ 'layout-backtop': isFixedHeader }">
+        <layoutHeader v-if="!isFixedHeader" />
+        <layoutMain />
+      </el-scrollbar>
+    </el-container>
+    <el-backtop target=".layout-backtop .el-scrollbar__wrap"></el-backtop>
+  </el-container>
 </template>
 
 <script lang="ts">
-import { computed, getCurrentInstance, watch, defineComponent } from 'vue';
+import {computed, getCurrentInstance, watch, defineComponent, ref} from 'vue';
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useThemeConfig } from '/@/stores/themeConfig';
-import Aside from '/@/layout/component/aside.vue';
-import Header from '/@/layout/component/header.vue';
-import Main from '/@/layout/component/main.vue';
+import layoutAside from '/@/layout/component/aside.vue';
+import layoutHeader from '/@/layout/component/header.vue';
+import layoutMain from '/@/layout/component/main.vue';
 
 export default defineComponent({
-	name: 'layoutDefaults',
-	components: { Aside, Header, Main },
-	setup() {
-		const { proxy } = <any>getCurrentInstance();
-		const route = useRoute();
-		const storesThemeConfig = useThemeConfig();
-		const { themeConfig } = storeToRefs(storesThemeConfig);
-		const isFixedHeader = computed(() => {
-			return themeConfig.value.isFixedHeader;
-		});
-		// 监听路由的变化
-		watch(
-			() => route.path,
-			() => {
-				proxy.$refs.layoutDefaultsScrollbarRef.wrap$.scrollTop = 0;
-			}
-		);
-		return {
-			isFixedHeader,
-		};
-	},
+  name: 'layoutDefaults',
+  components: { layoutAside, layoutHeader, layoutMain },
+  setup() {
+    const route = useRoute();
+    const layoutDefaultsScrollbarRef = ref()
+    const storesThemeConfig = useThemeConfig();
+    const { themeConfig } = storeToRefs(storesThemeConfig);
+    const isFixedHeader = computed(() => {
+      return themeConfig.value.isFixedHeader;
+    });
+    // 监听路由的变化
+    watch(
+        () => route.path,
+        () => {
+          layoutDefaultsScrollbarRef.value.wrapRef.scrollTop = 0;
+        }
+    );
+    return {
+      isFixedHeader,
+      layoutDefaultsScrollbarRef,
+    };
+  },
 });
 </script>
