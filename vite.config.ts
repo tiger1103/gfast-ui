06402c64d01a7cc0ea1d6d7018745712f04b52c7
yxh <yxh1103@qq.com>
2023-08-18 08:49:28 +0800

    fix 修复语言包找不到问题

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 		base: mode.command === 'serve' ? './' : env.VITE_PUBLIC_PATH,
 		hmr: true,
 		optimizeDeps: {
-			include: ['element-plus/lib/locale/lang/zh-cn', 'element-plus/lib/locale/lang/en', 'element-plus/lib/locale/lang/zh-tw'],
+			include: ['element-plus/es/locale/lang/zh-cn', 'element-plus/es/locale/lang/en', 'element-plus/es/locale/lang/zh-tw'],
 		},
 		server: {
 			host: '0.0.0.0',
