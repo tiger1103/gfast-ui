60852f3ebea779a43f3b4ca3cfd4385f20db304a
yxh <yxh1103@qq.com>
2023-12-14 10:00:05 +0800

    fix 修复字体图表cdn失效改为本地路径

--- src/utils/setIconfont.ts
+++ src/utils/setIconfont.ts
@@ -1,7 +1,7 @@
 // 字体图标 url
 const cssCdnUrlList: Array<string> = [
-	'//at.alicdn.com/t/font_2298093_y6u00apwst.css',
-	'//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+	import.meta.env.BASE_URL+'at.alicdn.com/t/font_2298093_y6u00apwst.css',
+	import.meta.env.BASE_URL+'netdna.bootstrapcdn.com/css/font-awesome.min.css',
 ];
 // 第三方 js url
 const jsCdnUrlList: Array<string> = [];
